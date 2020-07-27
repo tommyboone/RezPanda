3e72a47d2e0b9f39349c8ef3a69cfdc14d4a5fc7
Tommy Boone <tommyboone23@amazon-c54d2ce8d.localdomain>
2020-07-27 09:57:08 -0500

    added mongoose model

--- api/models/slot.js
+++ api/models/slot.js
@@ -1,11 +1,13 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+model = mongoose.model.bind(mongoose);
 
 const slotSchema = new Schema({
   slot_time: { type: String, required: true },
   slot_date: { type: String, required: true },
   created_at: Date,
 });
-let Slot = mongoose.model("Slot", slotSchema);
+const Slot = model("Slot", slotSchema);
 
 module.exports = Slot;
